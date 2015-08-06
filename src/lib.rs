7d2498c25f5b805173a468b5e089c1a4e418f14b
Sam Rijs <srijs@airpost.net>
2015-08-06 19:47:57 +0200

    more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 /// A comonoid in a monoidal category is a monoid
-/// in the dual category.
+/// in the dual category, what is the problem?
 ///
 /// It is useful within Rust's ownership type system,
 /// representing an object that can be both cloned
 /// and destroyed.
 pub trait Comonoid {
+    /// The dual to the monoidal unit.
+    /// It takes ownership of a value and returns `()`,
+    /// rendering it unusable.
     fn counit(self) -> ();
+    /// The dual to the monoidal multiplication.
+    /// It takes ownership of a value and returns a tuple of
+    /// values, effectively performing a clone operation.
     fn comult(self) -> (Self, Self);
 }
 
