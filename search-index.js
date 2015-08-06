893c0d48a4818d7ad678b49196ed6f13845361df
Sam Rijs <srijs@airpost.net>
2015-08-06 18:59:16 +0200

    add docs

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['comonoid'] = {"items":[[0,"","comonoid","",null,null],[8,"Comonoid","","A comonoid in a monoidal category is a monoid\nin the dual category.",null,null],[10,"counit","","",0,null],[10,"comult","","",0,null]],"paths":[[8,"Comonoid"]]};
+initSearch(searchIndex);
